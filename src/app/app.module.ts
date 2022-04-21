87d4a0354024017b1eb9c43b4ab87a4f53cc47fa
shforeal <rivena14798@gmail.com>
2022-04-21 22:12:26 +0300

    'first'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './cmps/contact-list/contact-list.component';
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
+import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component';
+import { BitcoinAppComponent } from './pages/bitcoin-app/bitcoin-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactPreviewComponent,
+    ContactFilterComponent,
+    BitcoinAppComponent
   ],
   imports: [
     BrowserModule,
