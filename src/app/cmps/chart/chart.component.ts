561558e8c6905281f1284ed537b3f8f7621692a4
shforeal <rivena14798@gmail.com>
2022-04-22 18:32:58 +0300

    gets chart data from bitcoin api

--- src/app/cmps/chart/chart.component.ts
+++ src/app/cmps/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BitcoinService } from 'src/app/services/bitcoin.service.service';
 
 @Component({
   selector: 'chart',
@@ -6,49 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-
+  
+  //test
   saleData = [
-    { name: "Mobiles", value: 105000 },
-    { name: "Laptop", value: 55000 },
-    { name: "AC", value: 15000 },
-    { name: "Headset", value: 150000 },
-    { name: "Fridge", value: 20000 }
+    {name: '11/23/2021', value: 494762556.34959996},
+    {name: '11/24/2021', value: 480199475.88900006},
+     {name: '11/25/2021', value: 332409438.7548},
+     {name: '11/26/2021', value: 289094884.176},
+    {name: '11/27/2021', value: 777492583.0165},
+     {name: '11/28/2021', value: 277078998.492},
+     {name: '11/29/2021', value: 252736299.37800002},
+     {name: '11/30/2021', value: 367826530.49249995},
+     {name: '12/1/2021', value: 522506214.22769994},
+     {name: '12/2/2021', value: 479668944.2496},
+     {name: '12/3/2021', value: 431885271.2832}
+    // { name: "Mobiles", value: 105000 },
+    // { name: "Laptop", value: 55000 },
+    // { name: "AC", value: 15000 },
+    // { name: "Headset", value: 150000 },
+    // { name: "Fridge", value: 20000 }
   ];
-  // view: any[] = [700, 300];
 
-  // // options
-  // legend: boolean = true;
-  // showLabels: boolean = true;
-  // animations: boolean = true;
-  // xAxis: boolean = true;
-  // yAxis: boolean = true;
-  // showYAxisLabel: boolean = true;
-  // showXAxisLabel: boolean = true;
-  // xAxisLabel: string = 'Year';
-  // yAxisLabel: string = 'Population';
-  // timeline: boolean = true;
-
-  // colorScheme = {
-  //   domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-  // };
-
-  // constructor() {
-  //   Object.assign(this, {  multi:this.saleData });
-  // }
-
-  // onSelect(data): void {
-  //   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onActivate(data): void {
-  //   console.log('Activate', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onDeactivate(data): void {
-  //   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  // }
- 
-  ngOnInit(): void {
-    
+  trades: [] = null
+  constructor(private bitcoinService: BitcoinService){}
+  async ngOnInit(): Promise<void> {
+    //console.log('type', !!this.bitcoinService.getTradeVolume().subscribe); // check if we got observable
+    if(!this.bitcoinService.getTradeVolume().subscribe) this.trades = this.bitcoinService.getTradeVolume()
+    else this.bitcoinService.getTradeVolume().subscribe(data => {
+      // console.log('data', data);
+      this.trades = data
+    })
   }
 }
+
+ 
+
